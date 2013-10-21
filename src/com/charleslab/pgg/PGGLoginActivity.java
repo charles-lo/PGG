4bc287e8fcf5d3cef8a74f585c947583bc385377
Charles <jerries.lo@gmail.com>
2013-10-22 02:24:30 +0800

    update login page

--- src/com/charleslab/pgg/PGGLoginActivity.java
+++ src/com/charleslab/pgg/PGGLoginActivity.java
@@ -1,7 +1,15 @@
 package com.charleslab.pgg;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class PGGLoginActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -9,5 +17,23 @@ public class PGGLoginActivity extends Activity {
 	   public void onCreate(Bundle savedInstanceState) {
 	      super.onCreate(savedInstanceState);
 	      setContentView(R.layout.login);
+	      ActionBar actionBar = getActionBar(); 
+	      actionBar.setDisplayShowTitleEnabled(false); 
+	      actionBar.setDisplayShowHomeEnabled(false);
+	      //
+	      DisplayMetrics metrics = new DisplayMetrics();  
+	      final WindowManager w = (WindowManager) getSystemService(WINDOW_SERVICE);
+	      final Display d = w.getDefaultDisplay();
+	      d.getMetrics(metrics);
+	      int size = metrics.widthPixels * 4 / 10;
+	      //
+	      ImageButton btnFacebook = ((ImageButton)findViewById(R.id.btn_facebook));
+	      ImageButton btnPGG = ((ImageButton)findViewById(R.id.btn_pgg));
+	      RelativeLayout.LayoutParams params = (LayoutParams) btnFacebook.getLayoutParams();
+	      params.width = params.height = size;
+	      btnFacebook.setLayoutParams(params);
+	      params = (LayoutParams) btnPGG.getLayoutParams();
+	      params.width = params.height = size;
+	      btnPGG.setLayoutParams(params);
 	   }
 }
